fabcdd2d4c91fde52e6f78a88304795daa8a7fe9
SoShibby <hnilsson90@hotmail.com>
2015-04-01 22:56:35 +0200

    Added notifications drop down menu

---
+++ client/views/desktop/commons/notifications/notifications.js
@@ -0,0 +1,42 @@
+﻿//Display old notifications
+Template.Notifications.oldNotifications = function() {
+	//Populate the notifications with dummy data, just so we can see how the website will look
+	return [{
+				description: "Overall temperature has been automatically risen to 23°C",
+				date: 1427919995000
+			},
+			{
+				description: "The washing machine was left on while nobody was home",
+				date: 1420143995000
+			},
+			{
+				description: "Dishwasher has finished the cycle",
+				date: 1417465595000
+			}];
+}
+
+//Convert dates from unix epoch date to a human readable date format
+Template.Notifications.date = function() {
+	return moment(this.date).format('h:mm MM MMMM YYYY');
+}
+
+//Show the notifications drop down menu when the user clicks on a dom element
+$('html').on('click', '.js-show-notifications', function() {
+	console.log('clicked');
+	$('#notifications').show();
+	$('#notifications .background').fadeIn();
+	$('#notifications .drop-down').animate({
+		top: "0",
+	}, 1000);
+});
+
+//Hide the notifications drop down menu when the user clicks on a dom element
+$('html').on('click', '#notifications .js-close', function() {
+	$('#notifications .drop-down').animate({
+		top: "-100%",
+	}, 1000, function() {
+		$('#notifications .background').fadeOut(400, function() {
+			$('#notifications').hide();
+		});
+	});		
+});
