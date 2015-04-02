226edb8eebbe176f4e131614a3f2b280d656108f
SoShibby <hnilsson90@hotmail.com>
2015-04-02 20:58:28 +0200

    Added 'Locations' collection and added coordinates to devices
    Populated the 'Locations' collection with some temporary data and added
    some dummy location data to devices.

--- client/lib/collections/collections.js
+++ client/lib/collections/collections.js
@@ -1,4 +1,5 @@
 Devices = new Meteor.Collection(null);
+Locations = new Meteor.Collection(null);
 
 //Insert "dummy" device data for now
 Devices.insert({ 
@@ -10,7 +11,11 @@ Devices.insert({
 													'brightness': 1,
 													'power': true
 												}
-								}]
+								}],
+					location: {
+								latitude: 40.78137,
+								longitude: -73.96590
+							  }
 				});
 				
 Devices.insert({ 
@@ -21,5 +26,25 @@ Devices.insert({
 									properties: {
 													'power': true
 												}
-								}]
+								}],
+					location: {
+								latitude: 40.78143,
+								longitude: -73.96716
+							  }
 				});
+				
+Locations.insert({
+					name: "Bedroom",
+					x1: -73.96616,
+					y1: 40.78154,
+					x2: -73.96561,
+					y2: 40.78115
+				 });
+				 
+Locations.insert({
+					name: "Kitchen",
+					x1: -73.96753,
+					y1: 40.78162,
+					x2: -73.96699,
+					y2: 40.78125
+				 });
