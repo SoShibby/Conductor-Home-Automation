b367cc7b7c0d373e4463b154bb61775646276dd7
SoShibby <hnilsson90@hotmail.com>
2015-04-03 02:08:57 +0200

    Added a side bar navigation menu to the 'Lights' page

---
+++ client/views/desktop/commons/sidebar/sidebar.js
@@ -0,0 +1,8 @@
+Template.Sidebar.helpers({
+	title: function() {
+		return this.title.toUpperCase();
+	},
+	selected: function() {
+		return this.selected ? "selected" : "";
+	}
+});
--- client/views/desktop/lights/lights.js
+++ client/views/desktop/lights/lights.js
@@ -7,5 +7,58 @@
 	},
 	isLightOn: function() {
 		return this.properties.power ? "active" : "";
+	},
+	sidebarInformation: function() {
+		return {
+					title: "Lighting Control",
+					links: makeNavigationLinks()
+			   };
 	}
 });
+
+/**
+ * makeNavigationLinks is used for creating links and link description for the side bar navigation
+ *
+ * @return  array containing link information that is shown in the side bar
+ */
+function makeNavigationLinks(){
+	var links = [];
+	var locations = Locations.find().fetch();
+	
+	for(var i = 0; i < locations.length; i++){
+		var location = locations[i];
+		
+		var components = ComponentFinder.find({
+												locationName: location.name,
+												componentType: 'light',
+												propertyName: 'power',
+												propertyValue: true,
+											 });
+		
+		var description = "";
+		var highlightedDescription = "";
+		
+		if(components.length === 0) {
+			description = "All lights are off";
+			highlightedDescription = "";
+		} else if(components.length === 1) {
+			description = " light is on";
+			highlightedDescription = "1";
+		} else {
+			description = " lights are on";
+			highlightedDescription = components.length;
+		}
+		
+		links.push({
+						name: location.name,
+						highlightedDescription: highlightedDescription,
+						description: description,
+						url: Router.current().route.url() + "?location=" + location.name,
+						selected: false
+				   });
+	}
+	
+	return links;
+}
+
+
