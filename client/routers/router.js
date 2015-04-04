792da0827960b0a65322588f4e1dcbd1a4fa4cca
SoShibby <hnilsson90@hotmail.com>
2015-04-05 01:22:52 +0200

    Subscribing to the 'Device' and 'Locations' data in the 'lights' route
    Also waiting for the subscriptions to finish before we render the page.

--- client/routers/router.js
+++ client/routers/router.js
@@ -11,5 +11,16 @@ Router.route('lights', {
 				devices: DeviceFinder.find({ locationName: this.params.query.locationName }),	//Return only devices that are in the locationNames location
 				locationName: this.params.query.locationName 
 			}
-		  }
+		  },
+	subscriptions: function() {
+		return [
+					Meteor.subscribe('devices'),
+					Meteor.subscribe('locations')
+				];
+	},
+	action: function () {
+		if (this.ready()) {
+			this.render();
+		}
+	}
 });
