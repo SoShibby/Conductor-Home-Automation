d7527ca3c05532c97fcba9672c0553c211739035
SoShibby <hnilsson90@hotmail.com>
2015-04-06 22:58:19 +0200

    Fixed deprecated template helper format
    Fixed "Warning: Assigning helper with `Template.Home.people = ...` is
    deprecated.  Use `Template.Home.helpers(...)` instead." message that
    shows up in the console.
    https://www.meteor.com/blog/2014/10/13/meteor-094-mobile-and-testing

--- client/views/desktop/home/home.js
+++ client/views/desktop/home/home.js
@@ -1,17 +1,18 @@
-Template.Home.people = function(){
-    //Adding some dummy data just to see how the website will look.
-    return [{
-                firstName: "John",
-                lastName: "Doe"
-            },
-            {
-                firstName: "Jane",
-                lastName: "Doe"
-            }];
-}
-
-Template.Home.selected = function(){
-    //Make the person John Doe selected, just so we can see how the website will look.
-    if(this.firstName === "John" && this.lastName === "Doe")
-        return "selected";
-}
+Template.Home.helpers({
+    people: function() {
+        //Adding some dummy data just to see how the website will look.
+        return [{
+                    firstName: "John",
+                    lastName: "Doe"
+                },
+                {
+                    firstName: "Jane",
+                    lastName: "Doe"
+                }];
+    },
+    selected: function() {
+        //Make the person John Doe selected, just so we can see how the website will look.
+        if(this.firstName === "John" && this.lastName === "Doe")
+            return "selected";
+    }
+});
