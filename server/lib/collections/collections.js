0c986f855c2a1170b49c7db427e7d00cf3db1a6c
SoShibby <hnilsson90@hotmail.com>
2015-04-08 14:05:41 +0200

    Added missing 'ControlUnits' collection

--- server/lib/collections/collections.js
+++ server/lib/collections/collections.js
@@ -1,3 +1,4 @@
 Devices = new Meteor.Collection("devices");
 Locations = new Meteor.Collection("locations");
 ApiKeys = new Meteor.Collection("apiKeys");
+ControlUnits = new Meteor.Collection("controlUnits");
