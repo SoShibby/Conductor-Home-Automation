684420c713f50c9be464ebe51bc7bbd7d0120b64
SoShibby <hnilsson90@hotmail.com>
2015-05-01 20:57:24 +0200

    Publishing friends and all user information to the client

--- server/publications/publications.js
+++ server/publications/publications.js
@@ -9,3 +9,22 @@ Meteor.publish("locations", function () {
 Meteor.publish("controlUnits", function () {
     return ControlUnits.find();
 });
+
+Meteor.reactivePublish("friends", function () {
+	if(this.userId){
+		var user = Meteor.users.findOne({ _id: this.userId }, {reactive: true});
+        
+        var friends = [];
+        friends = friends.concat(user.friends.confirmed);
+        friends = friends.concat(user.friends.unconfirmed);
+        friends = friends.concat(user.friends.requests);        
+		
+        return Meteor.users.find({ _id: { $in: friends } }, { fields: { friends: 0, services: 0 }});
+	}
+});
+
+Meteor.publish("myProfile", function () {
+	if(this.userId){
+		return Meteor.users.find({ _id: this.userId }, { fields: { services: 0 }});
+	}
+});
