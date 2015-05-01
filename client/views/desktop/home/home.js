44f272c3d91311efb23299754a97f966cbd8b714
SoShibby <hnilsson90@hotmail.com>
2015-05-01 21:45:53 +0200

    When a user portrait is clicked on the homepage then make the user selected

--- client/views/desktop/home/home.js
+++ client/views/desktop/home/home.js
@@ -11,3 +11,9 @@ Template.Home.onCreated(function() {
     //Set the currently logged in user as the default selected user when the homepage is rendered
     selectedUser.set(Meteor.user());
 });
+
+Template.Home.events({
+    'click .person': function(event) {
+        selectedUser.set(this);
+    }
+});
