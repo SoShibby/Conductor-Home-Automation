16da27cae2becf57b1b8f018e0f301306b3b5efb
SoShibby <hnilsson90@hotmail.com>
2015-05-02 17:23:36 +0200

    Added messagebox library

---
+++ client/views/desktop/commons/messagebox/messagebox.js
@@ -0,0 +1,30 @@
+var Messages = new Meteor.Collection(null);
+
+$(document).ready(function() {
+    MessageBox.init();
+});
+
+MessageBox = {
+    init: function(){
+        var domRoot = $('body')[0];
+		Blaze.render(Template.MessageBox, domRoot);
+        console.log('rendered');
+        console.log(domRoot);
+    },
+	displayInfo: function(title, message){
+		Messages.insert({ title: title, body: message });
+	},
+	displayError: function(title, message){
+		Messages.insert({ title: title, body: message });
+	}
+}
+
+Template.MessageBox.message = function(){
+	return Messages.findOne();
+}
+
+Template.MessageBox.events({
+	'click .js-close': function(event){
+		Messages.remove(this._id);
+	}
+});
