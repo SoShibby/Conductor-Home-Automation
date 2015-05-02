76274de58891c1d167646eb113abd890322ae524
SoShibby <hnilsson90@hotmail.com>
2015-05-03 00:37:53 +0200

    Display the owner of the control unit in a human readable way

--- client/views/desktop/commons/viewControlUnit/viewControlUnit.js
+++ client/views/desktop/commons/viewControlUnit/viewControlUnit.js
@@ -1,3 +1,10 @@
+Template.ViewControlUnit.helpers({
+    'owner': function() {
+        var user = Meteor.users.findOne({ _id: this.owner });
+        return Format('{firstName} {lastName} ({emails.0.address})', user); 
+    }
+});
+
 Template.ViewControlUnit.events({
     'click .js-close': function(event, template) {
         Blaze.remove(template.view);
