31bbc5ea3acc61efe7c47c7de4f1d10919d07701
SoShibby <hnilsson90@hotmail.com>
2015-05-03 17:18:46 +0200

    Added 'add friend' popup window

---
+++ client/views/desktop/commons/addFriend/addFriend.js
@@ -0,0 +1,19 @@
+Template.AddFriend.events = {
+	'click .js-add': function(event, template){
+		var form = template.find('form');
+		var formData = $(form).serializeObject();
+		
+		Meteor.call('addFriend', formData.email, function(error, result){
+			if(error){
+				MessageBox.displayInfo("Failed to add a new friend", "An error occurred when adding a new friend. The error message was: " + error);
+			}else{
+				Blaze.remove(template.view);
+				$(template.firstNode).remove();
+			}
+		});
+	},
+	'click .js-close': function(event, template){
+		Blaze.remove(template.view);
+		$(template.firstNode).remove();
+	}
+}
