f5b89ab5a9df208636f4cb07387f3332f2223ef7
SoShibby <hnilsson90@hotmail.com>
2015-05-03 16:01:39 +0200

    Show all the components that a device contains directly in the 'view device' popup window

--- client/views/desktop/commons/viewDevice/viewDevice.js
+++ client/views/desktop/commons/viewDevice/viewDevice.js
@@ -1,6 +1,12 @@
 Template.ViewDevice.events({
+    /* Close pop up window */
     'click .js-close': function(event, template) {
         Blaze.remove(template.view);
 		$(template.firstNode).remove();
-    }
+    },
+    /* View component information */
+    'click .js-info': function(event, template){
+		var domRoot = $('body')[0];
+		Blaze.renderWithData(Template.ViewComponent, this, domRoot);
+	}
 });
