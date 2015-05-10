cd3f22381542a0d0a63c5ab0a5f0b124e1d09c91
SoShibby <hnilsson90@hotmail.com>
2015-05-10 19:23:54 +0200

    Added method for sharing google calendar

---
+++ server/methods/calendarMethods.js
@@ -0,0 +1,12 @@
+Meteor.methods({
+    shareCalendar: function(email){
+        if(!User.isUserLoggedIn(this.userId))
+            throw new Meteor.Error(ErrorCode.UNAUTHORIZED);
+        
+        Assert.validEmail(email, "Invalid email address.");
+        
+        var calendarId = CalendarFacade.getCalendarId(this.userId);
+        
+        CalendarFacade.shareCalendar(this.userId, email);
+    }
+});
