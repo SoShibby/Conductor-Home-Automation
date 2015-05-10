f95f24e2e60e1c4fa7ba658cced124c1396e2390
SoShibby <hnilsson90@hotmail.com>
2015-05-10 19:01:54 +0200

    Added server method for sending command to control units from clients

---
+++ server/methods/commandMethods.js
@@ -0,0 +1,16 @@
+Meteor.methods({
+    sendCommand: function(filter, value){
+        if(!User.isUserLoggedIn(this.userId))
+            throw new Meteor.Error(ErrorCode.UNAUTHORIZED);
+            
+        Assert.isObject(filter, "The parameter named 'filter' must be an object.");
+        Assert.isObject(value, "The parameter named 'value' must be an object.");
+        
+        filter.userAccess = this.userId;    // We need to filter devices based on user access, so the user can only send commands to the devices that he/she got access to.
+        
+        Controller.send({ 
+                            filter: filter, 
+                            value: value 
+                        });
+    }
+});
