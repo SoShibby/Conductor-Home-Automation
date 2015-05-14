b36d04454c2711a98236bdd1084f3916bdb2ccc2
SoShibby <hnilsson90@hotmail.com>
2015-05-14 11:35:55 +0200

    Added library 'CalendarMapping'
    This library is used for mapping an event in google calendar with a
    command that should be sent to control units when triggered.

--- client/lib/collections/collections.js
+++ client/lib/collections/collections.js
@@ -2,3 +2,4 @@ Devices = new Meteor.Collection("devices");
 Locations = new Meteor.Collection("locations");
 ControlUnits = new Meteor.Collection("controlUnits");
 Feeds = new Meteor.Collection("feeds");
+CalendarMappings = new Meteor.Collection("calendarMappings");
--- server/lib/collections/collections.js
+++ server/lib/collections/collections.js
@@ -3,3 +3,4 @@ Locations = new Meteor.Collection("locations");
 ApiKeys = new Meteor.Collection("apiKeys");
 ControlUnits = new Meteor.Collection("controlUnits");
 Feeds = new Meteor.Collection("feeds");
+CalendarMappings = new Meteor.Collection("calendarMappings");
