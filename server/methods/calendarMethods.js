0c9171d7fe1fddf2527ff87aa9b32fbb59456fc3
SoShibby <hnilsson90@hotmail.com>
2015-05-14 11:44:22 +0200

    Added server methods for adding/removing calendar event mappings

--- server/methods/calendarMethods.js
+++ server/methods/calendarMethods.js
@@ -14,5 +14,25 @@ Meteor.methods({
             throw new Meteor.Error(ErrorCode.UNAUTHORIZED);
         
         CalendarFacade.refreshEvents(this.userId);
+    },
+    addCalendarEventMapping: function(eventName, command, timeOffset) {
+        if(!User.isUserLoggedIn(this.userId))
+            throw new Meteor.Error(ErrorCode.UNAUTHORIZED);
+        
+        Assert.isString(eventName, "Parameter 'event name' must be a string value");
+        Assert.isInt(timeOffset, "Parameter 'time offset' must be a int value");
+        
+        // Verify that the command parameter is a valid command
+        
+        CalendarMapping.add(this.userId, eventName, command, timeOffset);
+    },
+    removeCalendarEventMapping: function(mappingId) {
+        if(!User.isUserLoggedIn(this.userId))
+            throw new Meteor.Error(ErrorCode.UNAUTHORIZED);
+        
+        if(!CalendarMapping.isOwner(this.userId, mappingId))
+            throw new Meteor.Error(ErrorCode.UNAUTHORIZED, "Only the owner of the calendar mapping can remove it.");
+            
+        CalendarMapping.remove(mappingId);
     }
 });
