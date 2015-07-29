a48d5c83db863a1fed9b76ca1d861dec83d5b3a8
antraxx <hnilsson90@hotmail.com>
2015-07-29 12:05:27 +0200

    Refactored the code in viewComponent to use _.find instead of _.filter

--- client/views/desktop/commons/viewComponent/viewComponent.js
+++ client/views/desktop/commons/viewComponent/viewComponent.js
@@ -3,27 +3,28 @@ var currentDate = new ReactiveVar(new Date());
 Template.ViewComponent.helpers({
     component: function() {
         var device = Devices.findOne({ 'components.id': this.componentId });
-        
+
         if(!device) {   //If no device was found containing the component that we are looking for then just return an empty object
             return {};
         }
-        
-        device.components.filter(function(component, index, array) {
-            return (component.id === this.componentId);
+
+        var componentId = this.componentId;
+        var component = _.find(device.components, function(component) {
+            return component.id === componentId;
         });
-        
-        return (device.components.length > 0) ? device.components[0] : {};
+
+        return component;
     },
     properties: function() {
         //Blaze can't loop through object values, so we need to convert it into an array first
         var result = [];
-        
-        for (var propertyName in this.properties) {      
+
+        for (var propertyName in this.properties) {
             var obj = this.properties[propertyName];
             obj.name = propertyName;
             result.push(obj);
         }
-        
+
         return result;
     },
     value: function() {
@@ -32,12 +33,12 @@ Template.ViewComponent.helpers({
     methods: function() {
         //Blaze can't loop through object values, so we need to convert it into an array first
         var result = [];
-        
-        for (var methodName in this.methods) {     
+
+        for (var methodName in this.methods) {
             var method = {};
             method.name = methodName;
             method.parameters = [];
-            
+
             var parameters = this.methods[methodName].parameters;
             for (var parameterName in parameters) {
                 var parameter = {};
@@ -45,10 +46,10 @@ Template.ViewComponent.helpers({
                 parameter.dataType = parameters[parameterName];
                 method.parameters.push(parameter);
             }
-            
+
             result.push(method);
         }
-        
+
         return result;
     },
     updated: function() {
@@ -57,8 +58,8 @@ Template.ViewComponent.helpers({
         //We use a reactive variable to get the current date,
         //this is done because we want the date to update every seconds,
         //and if we would have just used 'new Date()' it would have just updated the date once
-        var dateNow = moment(currentDate.get());    
-        
+        var dateNow = moment(currentDate.get());
+
         return updated.from(dateNow);   //Get the time difference between the current date and the date the property was last updated
     }
 });
