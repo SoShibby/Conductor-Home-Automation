bdf40d1450e5a20d609056494f5ff304cc90d6c5
antraxx <hnilsson90@hotmail.com>
2015-08-21 22:39:50 +0200

    Converted the notifications popup from blazer to angular

--- client/layouts/desktop/header.js
+++ client/layouts/desktop/header.js
@@ -4,11 +4,24 @@ directive('header', function() {
         restrict: 'A',
         templateUrl: "client/layouts/desktop/header.ng.html",
         scope: {},
-        controller: ['$scope', '$interval', function($scope, $interval) {
+        controller: ['$scope', '$interval', 'ModalService', function($scope, $interval, ModalService) {
             $interval(function() {
                 $scope.time = moment().format('hh:mmA');
                 $scope.date = moment().format('M MMMM YYYY');
             }, 1000);
+
+            $scope.showNotifications = function() {
+                console.log('show notifications');
+
+                ModalService.showModal({
+                    templateUrl: 'client/views/desktop/commons/notifications/notifications.ng.html',
+                    controller: 'notifications',
+                }).then(function(modal) {
+                    modal.close.then(function() {
+
+                    });
+                });
+            }
         }]
     }
 });
--- client/views/desktop/commons/notifications/notifications.js
+++ client/views/desktop/commons/notifications/notifications.js
@@ -1,40 +1,17 @@
-﻿Template.Notifications.helpers({
-    oldNotifications: function() {
-        //Populate the notifications with dummy data, just so we can see how the website will look
-        return [{
-                    description: "Overall temperature has been automatically risen to 23°C",
-                    date: 1427919995000
-                },
-                {
-                    description: "The washing machine was left on while nobody was home",
-                    date: 1420143995000
-                },
-                {
-                    description: "Dishwasher has finished the cycle",
-                    date: 1417465595000
-                }];
-    },
-    date: function() {
-        return moment(this.date).format('h:mm MM MMMM YYYY');
-    }
-});
+﻿angular.module('homeautomation')
+    .controller('notifications', ['$scope', '$stateParams', '$meteor', 'close',
+        function($scope, $stateParams, $meteor, close) {
+            $scope.close = close;
 
-//Show the notifications drop down menu when the user clicks on a dom element
-$('html').on('click', '.js-show-notifications', function() {
-    $('#notifications').show();
-    $('#notifications .background').fadeIn();
-    $('#notifications .drop-down').animate({
-        top: "0",
-    }, 1000);
-});
-
-//Hide the notifications drop down menu when the user clicks on a dom element
-$('html').on('click', '#notifications .js-close', function() {
-    $('#notifications .drop-down').animate({
-        top: "-100%",
-    }, 1000, function() {
-        $('#notifications .background').fadeOut(400, function() {
-            $('#notifications').hide();
-        });
-    });
-});
+            $scope.notifications = [{
+                description: "Overall temperature has been automatically risen to 23°C",
+                date: 1427919995000
+            }, {
+                description: "The washing machine was left on while nobody was home",
+                date: 1420143995000
+            }, {
+                description: "Dishwasher has finished the cycle",
+                date: 1417465595000
+            }];
+        }
+    ]);
