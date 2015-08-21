4d0d5f9df49e3c99167c5023364e52215dd36e46
antraxx <hnilsson90@hotmail.com>
2015-08-21 21:10:25 +0200

    Made the JSON objects more readable in the server publication file

--- server/publications/publications.js
+++ server/publications/publications.js
@@ -1,50 +1,108 @@
-Meteor.publish("devices", function () {
-    if(this.userId){
-        return Devices.find({ userAccess: { $in: [ this.userId ] } });
+Meteor.publish("devices", function() {
+    if (this.userId) {
+        return Devices.find({
+            userAccess: {
+                $in: [this.userId]
+            }
+        });
     }
 });
 
-Meteor.publish("locations", function () {
-    return Locations.find();
-});
-
-Meteor.publish("controlUnits", function () {
-    return ControlUnits.find();
-});
-
-Meteor.reactivePublish("confirmedFriends", function () {
-    if(this.userId){
-        var user = Meteor.users.findOne({ _id: this.userId }, {reactive: true});
-        return Meteor.users.find({ _id: { $in: user.friends.confirmed } }, { fields: { friends: 0 }});
+Meteor.publish("locations", function() {
+    if (this.userId) {
+        return Locations.find({
+            creator: this.userId
+        });
     }
 });
 
-Meteor.reactivePublish("unconfirmedFriends", function () {
-    if(this.userId){
-        var user = Meteor.users.findOne({ _id: this.userId }, {reactive: true});
-        return Meteor.users.find({ _id: { $in: user.friends.unconfirmed } }, { fields: { friends: 0 }});
+Meteor.publish("controlUnits", function() {
+    if (this.userId) {
+        return ControlUnits.find({
+            owner: this.userId
+        });
     }
 });
 
-Meteor.reactivePublish("friendRequests", function () {
-    if(this.userId){
-        var user = Meteor.users.findOne({ _id: this.userId }, {reactive: true});
-        return Meteor.users.find({ _id: { $in: user.friends.requests } }, { fields: { friends: 0 }});
+Meteor.reactivePublish("confirmedFriends", function() {
+    if (this.userId) {
+        var user = Meteor.users.findOne({
+            _id: this.userId
+        }, {
+            reactive: true
+        });
+        return Meteor.users.find({
+            _id: {
+                $in: user.friends.confirmed
+            }
+        }, {
+            fields: {
+                friends: 0
+            }
+        });
     }
 });
 
-Meteor.publish("myProfile", function () {
-    if(this.userId){
-        return Meteor.users.find({ _id: this.userId }, { fields: { services: 0 }});
+Meteor.reactivePublish("unconfirmedFriends", function() {
+    if (this.userId) {
+        var user = Meteor.users.findOne({
+            _id: this.userId
+        }, {
+            reactive: true
+        });
+        return Meteor.users.find({
+            _id: {
+                $in: user.friends.unconfirmed
+            }
+        }, {
+            fields: {
+                friends: 0
+            }
+        });
     }
 });
 
-Meteor.publish("feeds", function () {
-    if(this.userId){
-        return Feeds.find({ owner: this.userId });
+Meteor.reactivePublish("friendRequests", function() {
+    if (this.userId) {
+        var user = Meteor.users.findOne({
+            _id: this.userId
+        }, {
+            reactive: true
+        });
+        return Meteor.users.find({
+            _id: {
+                $in: user.friends.friendRequests
+            }
+        }, {
+            fields: {
+                friends: 0
+            }
+        });
     }
 });
 
-Meteor.publish("calendarMappings", function () {
-	return CalendarMappings.find({ owner: this.userId });
-}); 
+Meteor.publish("myProfile", function() {
+    if (this.userId) {
+        return Meteor.users.find({
+            _id: this.userId
+        }, {
+            fields: {
+                services: 0
+            }
+        });
+    }
+});
+
+Meteor.publish("feeds", function() {
+    if (this.userId) {
+        return Feeds.find({
+            owner: this.userId
+        });
+    }
+});
+
+Meteor.publish("calendarMappings", function() {
+    return CalendarMappings.find({
+        owner: this.userId
+    });
+});
