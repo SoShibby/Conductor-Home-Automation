b7ef1bdee2a59f115ba67265502f267c954d187e
antraxx <hnilsson90@hotmail.com>
2015-08-21 21:28:08 +0200

    Removed duplicate row in 'collections.js' on the server

--- server/lib/collections/collections.js
+++ server/lib/collections/collections.js
@@ -4,7 +4,7 @@ ApiKeys = new Meteor.Collection("apiKeys");
 ControlUnits = new Meteor.Collection("controlUnits");
 Feeds = new Meteor.Collection("feeds");
 CalendarMappings = new Meteor.Collection("calendarMappings");
-TriggerActions = new Meteor.Collection("triggerActions");TriggerActions = new Meteor.Collection("triggerActions");
+TriggerActions = new Meteor.Collection("triggerActions");
 
 Devices.allow({
     insert: function(userId, device) {
