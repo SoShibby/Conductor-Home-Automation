7b052a20cff3d6bd4ec52aaea023aa0091d1fa8f
antraxx <hnilsson90@hotmail.com>
2015-08-21 21:35:32 +0200

    Added the TriggerActions collection to the client

--- client/lib/collections/collections.js
+++ client/lib/collections/collections.js
@@ -3,3 +3,4 @@ Locations = new Mongo.Collection("locations");
 ControlUnits = new Mongo.Collection("controlUnits");
 Feeds = new Mongo.Collection("feeds");
 CalendarMappings = new Mongo.Collection("calendarMappings");
+TriggerActions = new Mongo.Collection("triggerActions");
